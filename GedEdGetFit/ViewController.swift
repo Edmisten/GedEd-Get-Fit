e2a34375cca0cce02f90d286ca32b4adca2e92b8
NWMSUGedEd <s524747@mail.nwmissouri.edu>
2017-03-06 11:15:57 -0600

    Initial Commit

---
+++ GedEdGetFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GedEdGetFit
+//
+//  Created by Edmisten,Jacob S on 3/6/17.
+//  Copyright © 2017 Edmisten,Jacob S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
